9fda0bc945f913bfaf8f5c1465d653bbf0d11565
Neenu Antony <106283713+neenz16@users.noreply.github.com>
2025-08-19 18:37:42 +0000

    testing 2

--- unsloth_zoo/vision_utils.py
+++ unsloth_zoo/vision_utils.py
@@ -28,9 +28,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-# from __future__ import annotations
+from __future__ import annotations
 
-print(" future")
+print(" future back")
 
 __all__ = [
     "process_vision_info",
